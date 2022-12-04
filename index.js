78092bdc3b5ed68e2316d82e17ef7f86d031fc33
roza <rozanawzad2001@gmail.com>
2022-12-04 22:10:42 +0300

    done js function  lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    //returns the number of blocks given a value
+    if (someValue<42){
+      return(42-someValue);
+    }
+    else {
+  
+    return(someValue-42);
+  }
+  }
+  
+  
+  
+  function distanceFromHqInFeet (someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+    // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+    //the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+  
+  function distanceTravelledInFeet(val1,val2){
+    if (val2>val1){
+      return((val2-val1)*264);
+    }
+    else
+      return((val1-val2)*264);
+  }
+  
+  
+  function calculatesFarePrice(start,destination){
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance<= 400){
+      return 0;
+    }
+    if (distance>=400 && distance<=2000){
+      return (distance-400)* 0.02;
+    }
+    else if(distance>=2000&& distance<=2500){
+      return 25;
+    }
+    else if (distance > 2500){
+      return "cannot travel that far";
+    }
+  }
